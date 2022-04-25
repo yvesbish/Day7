7d9c7cc70f8fdcc4d4745ad08db9f5949c8f5ebb
yvesbish <102938012+yvesbish@users.noreply.github.com>
2022-04-25 12:10:55 +0800

    Create Pyramid.js

---
+++ Pyramid.js
@@ -0,0 +1,19 @@
+
+var lines = 10;
+for ( var j = 0; j<lines; j++){
+for (var i = 0; i < j+1; i++){ 
+   document.write("#");
+}
+  document.write("<br/>")
+}
+
+
+var lines = 10;
+for ( var j = 0; j<lines; j++){
+for (var i = 0; i < lines-j-1; i++){ 
+   document.write("#");
+}
+  document.write("<br/>")
+}
+
+    
